31d8116dc39f23642d598a3926541f3274b70589
Mireia <66952824+miiree12lb@users.noreply.github.com>
2024-11-17 23:44:08 +0100

    footer

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 // @ts-ignore
 import './css/root.css';
+import Footer from "./Footer.jsx";
 
 function Root() {
     return (
-        <>
-            <h1>Video Gallery</h1>
-        </>
-    )
+        <BrowserRouter>
+            <div id="view">
+                <h1>Video Gallery</h1>
+            </div>
+            
+            <div id="footer">
+                <Footer />
+            </div>
+        </BrowserRouter>
+    );
 }
 
 const rootElement = document.getElementById("root");
